c7687aacc42c6e871b21f3aeb697a6673b66e343
SakshiKindre <sakshikindre14@gmail.com>
2026-08-01 03:37:15 +0530

    Connect frontend to Render backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://employee-management-system-mlz1.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
